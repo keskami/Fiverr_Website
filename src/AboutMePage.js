9325138506781bfe400038899bcb097b9f0ad759
keskami <kkak808@gmail.com>
2023-12-12 14:23:53 -0500

    skeleton layout

---
+++ src/AboutMePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AboutMePage.css'; // Create AboutMePage.css for styling
+
+const AboutMePage = () => {
+  return (
+    <div className="about-container">
+      <h2>About Me</h2>
+      <p>
+        Your journey with affiliate marketing, Fiverr freelancing, online marketing through Instagram and Google Ads, and the impact of being mentored by Adrian Brambila.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMePage;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
+import './App.css'; // Create App.css for styling
+
+import HomePage from './pages/HomePage';
+import AboutMePage from './pages/AboutMePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <nav>
+          <NavLink to="/" exact activeClassName="active-tab">
+            Home
+          </NavLink>
+          <NavLink to="/about" activeClassName="active-tab">
+            About Me
+          </NavLink>
+        </nav>
+        <Switch>
+          <Route path="/about" component={AboutMePage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
